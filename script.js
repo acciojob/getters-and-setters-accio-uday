056b4457c6b71720bdb43c348156fb8c09a775c3
accio-uday <udaymanideep2002@gmail.com>
2025-06-12 12:39:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 //complete this code
-class Person {}
+class Person {
+	const person = new Person("John", 25);
+	console.log(person.name);
+	console.log(person.age);
+}
 
-class Student extends Person {}
+class Student extends Person {
+	const student = new Student("John", 25);
+	student.study();
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	const teacher = new Teacher("John", 30);
+	teacher.teach();
+}
 
 // Do not change the code below this line
 window.Person = Person;
