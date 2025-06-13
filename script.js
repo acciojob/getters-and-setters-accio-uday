ac960a309b474a105cf301fe2624697efe6c8c57
accio-uday <udaymanideep2002@gmail.com>
2025-06-13 16:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
-//complete this code
+// Basic class code
 class Person {
-	constructor(John, 25){
-	this.name=name;
-	this.age=age;	
-}
+	constructor(name, age){
+	this._name = name;
+	this._age = age;	
 }
 
+	// Getter for name
+	get name(){
+		return this._name;
+	}
+
+	// Setter for age
+	set age(value){
+		return this._age;
+	}
+}
+
+//Sub class: Student
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying.`);
+	study() {
+		console.log(`${this.name} is studying`);
 	}
 }
 
+// Sub class: Teacher
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.John} is teaching.`);
+	teach() {
+		console.log(`${this.name} is teaching`);
 	}
 }
-
-const person = new Person("John", 25);
-console.log(person.name);
-console.log(person.age);
-
-const student = new Student("John", 25);
-student.study();
-
-const teacher = new Teacher("John", 30);
-teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
